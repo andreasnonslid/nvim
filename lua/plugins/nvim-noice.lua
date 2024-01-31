25e3de2fe75565080187e3f90105141662a4cf44
Andreas Håvardsen <andreas.havardsen@autostoresystem.com>
2024-01-31 15:35:53 +0100

    Add gitsigns, and format all files

--- lua/plugins/nvim-noice.lua
+++ lua/plugins/nvim-noice.lua
@@ -1,28 +1,26 @@
 return {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-    },
-    dependencies = {
-        "MunifTanjim/nui.nvim",
-    }
-    ,
-    config = function()
-        require("noice").setup({
-            lsp = {
-                override = {
-                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                    ["vim.lsp.util.stylize_markdown"] = true,
-                    ["cmp.entry.get_documentation"] = true,
-                },
-            },
-            presets = {
-                command_palette = true,
-                long_message_to_split = true,
-                inc_rename = false,
-                lsp_doc_border = false,
-            },
-        })
-        require("telescope").load_extension("noice")
-    end
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("noice").setup({
+			lsp = {
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
+			},
+			presets = {
+				command_palette = true,
+				long_message_to_split = true,
+				inc_rename = false,
+				lsp_doc_border = false,
+			},
+		})
+		require("telescope").load_extension("noice")
+	end,
 }
