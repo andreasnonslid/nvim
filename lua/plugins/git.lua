69de6d8338b3e9942970c464e560521b8c6a7a51
Andreas Nonslid Håvardsen <andreas.nonshaav@hotmail.com>
2024-06-03 22:15:18 +0200

    Improve git workflow using diffview and neogit

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,15 +1,25 @@
 return {
     {
-        "lewis6991/gitsigns.nvim",
+		"lewis6991/gitsigns.nvim",
 
-        config = function()
-            require("gitsigns").setup()
+		config = function()
+			require("gitsigns").setup()
 
-            vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-            vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
-        end,
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+		end,
     },
+    {"sindrets/diffview.nvim" },
     {
-        "tpope/vim-fugitive",
-    },
+      "NeogitOrg/neogit",
+      dependencies = {
+        "nvim-lua/plenary.nvim",         -- required
+        "sindrets/diffview.nvim",        -- optional - Diff integration
+
+        -- Only one of these is needed, not both.
+        "nvim-telescope/telescope.nvim", -- optional
+        "ibhagwan/fzf-lua",              -- optional
+      },
+      config = true
+    }
 }
