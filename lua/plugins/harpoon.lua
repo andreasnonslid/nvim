d86faa6257adf27424a0d2fc1f40ee56c41ba10e
Andreas Nonslid Håvardsen <andreas.havardsen@autostoresystem.com>
2025-05-16 10:10:38 +0200

    fix harpoons

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -9,13 +9,13 @@ return {
 		vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "[A]dd harpoon mark." })
 		vim.keymap.set("n", "<C-y>", ui.toggle_quick_menu)
 
-		vim.keymap.set("n", "<C-h>", function()
+		vim.keymap.set("n", "<C-j>", function()
 			ui.nav_file(1)
 		end)
-		vim.keymap.set("n", "<C-j>", function()
+		vim.keymap.set("n", "<C-k>", function()
 			ui.nav_file(2)
 		end)
-		vim.keymap.set("n", "<C-k>", function()
+		vim.keymap.set("n", "<C-l>", function()
 			ui.nav_file(3)
 		end)
 	end,
